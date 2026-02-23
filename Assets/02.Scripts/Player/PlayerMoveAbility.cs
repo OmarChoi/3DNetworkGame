9d6faa66b8f60291886e102de115749c087cbe55
Omarchoi <omarchoi80@gmail.com>
2026-02-23 13:58:27 +0900

    feat: Player 이동 기능 추가

---
+++ Assets/02.Scripts/Player/PlayerMoveAbility.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerMoveAbility : MonoBehaviour
+{
+    public float MoveSpeed = 7f;
+    public float JumpForce = 2.5f;
+    public float RotationSpeed = 10f;
+
+    private const float GRAVITY = 30.0f;
+    private float _yVelocity = 0f;
+    
+    private CharacterController _characterController;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+    
+    private void Start()
+    {
+        _yVelocity = 0f;
+    }
+
+    private void Update()
+    {
+        Vector3 movement = GetMovement();
+        UpdateJump();
+
+        movement.y = _yVelocity;
+        _characterController.Move(movement * Time.deltaTime);
+    }
+
+    private Vector3 GetMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(h, 0, v);
+        return transform.TransformDirection(movement) * MoveSpeed;
+    }
+
+    private void UpdateJump()
+    {
+        if (Input.GetKey(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _yVelocity = JumpForce;
+        }
+        _yVelocity -= GRAVITY * Time.deltaTime;
+    }
+}
