116f4b506b3f11c30a93fde79324778f88c607da
Omarchoi <omarchoi80@gmail.com>
2026-02-23 15:34:23 +0900

    feat: 3가지 공격 기능 추가

--- Assets/02.Scripts/Player/PlayerAnimation.cs
+++ Assets/02.Scripts/Player/PlayerAnimation.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
     private static readonly int _moveSpeed = Animator.StringToHash("MoveSpeed");
+    private static readonly int _attackIndex = Animator.StringToHash("AttackIndex");
+
     private CharacterController _characterController;
     private Animator _animator;
     private PlayerStat _stat;
+    private EPlayerAttack _nextAttack;
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
         _stat = GetComponent<PlayerStat>();
+        PlayerAttackAbility.OnAttack += OnAttack;
     }
 
     private void Update()
@@ -32,4 +36,31 @@ public class PlayerAnimation : MonoBehaviour
         _animator.SetFloat(_moveSpeed, normalizedSpeed);
     }
 
+    private EPlayerAttack GetRandomAttack()
+    {
+        return (EPlayerAttack)Random.Range(0, (int)EPlayerAttack.Count);
+    }
+
+    private void OnAttack()
+    {
+        EPlayerAttack attack = GetRandomAttack();
+        if (_animator.GetInteger(_attackIndex) != (int)EPlayerAttack.None)
+        {
+            if (_nextAttack != EPlayerAttack.None) return;
+            _nextAttack = attack;
+            return;
+        }
+        _animator.SetInteger(_attackIndex, (int)attack);
+    }
+
+    private void OnAttackEnd()
+    {
+        if (_nextAttack != EPlayerAttack.None)
+        {
+            _animator.SetInteger(_attackIndex, (int)_nextAttack);
+            _nextAttack = EPlayerAttack.None;
+            return;
+        }
+        _animator.SetInteger(_attackIndex, (int)EPlayerAttack.None);
+    }
 }
